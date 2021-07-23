8d90af1a12d7c834e74940253e6c7fba99646c52
BrianKhaifah <briankhaifah@gmail.com>
2021-07-23 22:53:54 +0300

    Created anew profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RepositorieComponent } from './repositorie/repositorie.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RepositorieComponent
+    RepositorieComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
