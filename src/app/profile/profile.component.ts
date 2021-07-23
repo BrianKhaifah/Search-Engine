d4b7bd0d8d14f5bdf4149005863c7fc4e8adcb8f
BrianKhaifah <briankhaifah@gmail.com>
2021-07-24 00:42:12 +0300

    modified profile.ts/service

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../user'
+import { Repo } from '../repo'
+import { UserService } from '../user.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  Users:User
+  Repo : Repo [] = []
+
+  constructor( public usserHttpService:UserService) { }
 
   ngOnInit(): void {
+    this.searchGit ("")
+
+  }
+  searchGit ( searchTerm: string ) {
+    this.usserHttpService.searchGits ( searchTerm ).then (
+       (success ) => {
+         this.Users = this.usserHttpService.Users
+       },
+
+        (errors ) => {
+          console.log ( errors)
+        })
+
+        this.usserHttpService.searchRepos ( searchTerm ).then (
+
+          ( success ) => {
+
+            this.Repo = this.usserHttpService.Repo
+          } )
   }
 
 }
