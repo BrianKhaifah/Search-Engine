13b66dd1ab1a815d9b26bb397c25c6adfe177309
BrianKhaifah <briankhaifah@gmail.com>
2021-07-23 23:14:13 +0300

    modified class usser

--- src/app/usser.ts
+++ src/app/usser.ts
@@ -1,2 +1,17 @@
 export class Usser {
+
+  constructor (
+
+    public name: string,
+    public login: string,
+    public avator_url: any,
+    public followers: any,
+    public following: any,
+    public location: any
+    
+  ) {
+
+
+
+  }
 }
