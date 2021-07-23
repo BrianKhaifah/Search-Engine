b0ed8c0db754eea928d0c18b7c7da306ec1f0bf3
BrianKhaifah <briankhaifah@gmail.com>
2021-07-23 22:51:01 +0300

    Created anew repositorie component

---
+++ src/app/repositorie/repositorie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repositorie',
+  templateUrl: './repositorie.component.html',
+  styleUrls: ['./repositorie.component.css']
+})
+export class RepositorieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
